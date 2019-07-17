911eb570fbf9c962d0b578819b482eacd038ac84
starskystone <xautwl17@163.com>
2019-07-17 22:47:58 +0800

    增加一些内容

---
+++ how2j-servlet/src/com/how2j/servlet/RegisterServlet.java
@@ -0,0 +1,17 @@
+package com.how2j.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RegisterServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	
+}
